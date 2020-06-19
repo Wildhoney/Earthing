0ac5ed3b1a3f5c254e0d1bfe6435b12460b623d9
Wildhoney <adam.timberlake@gmail.com>
2020-06-20 00:49:20 +0100

    Added required PostGIS queries as comments

--- api/index.py
+++ api/index.py
@@ -3,6 +3,10 @@ from geopy import distance
 
 app = Flask(__name__)
 
+# SELECT ST_AsText(ST_MakeLine(ST_MakePoint(0.1278, 51.5074), ST_MakePoint(49.614232010011484, 39.202349184488774)))
+# SELECT ST_Expand(ST_GeomFromText(@linestring, 2163), 5)
+# SELECT ST_Intersects(polygon, @polygon::geometry);
+
 
 @app.route("/")
 def catch_all():
