e44ea8c59eee06eaf7ae56ef82a8326ed21342bc
Wildhoney <adam.timberlake@gmail.com>
2020-06-19 23:41:18 +0100

    Updated Python code to yield JSON

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask
 from geopy import distance
 
 app = Flask(__name__)
@@ -7,6 +7,5 @@ app = Flask(__name__)
 @app.route("/")
 def catch_all():
     position = distance.distance(miles=2500).destination((51.5074, 0.1278), 90)
-    return Response(
-        "%s, %s" % position.latitude, position.longitude, mimetype="text/html"
-    )
+
+    return {"lat": position.latitude, "lng": position.longitude}
