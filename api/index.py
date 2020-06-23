30610d91388cca8e062d6aeebbc45e1f80f53182
Wildhoney <adam.timberlake@gmail.com>
2020-06-23 18:20:57 +0100

    Setup .env for the API

--- api/index.py
+++ api/index.py
@@ -1,13 +1,24 @@
-from flask import Flask
+import os
 import psycopg2
+from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-conn = psycopg2.connect("")
+
+conn = psycopg2.connect(
+    "host={db_host} dbname={db_name} user={db_username} password={db_password}".format(
+        db_host=os.getenv("DB_HOST"),
+        db_name=os.getenv("DB_NAME"),
+        db_username=os.getenv("DB_USERNAME"),
+        db_password=os.getenv("DB_PASSWORD"),
+    )
+)
 
 
 @app.route("/<latitude>/<longitude>/<bearing>")
 def get(latitude, longitude, bearing):
-
     cursor = conn.cursor()
     cursor.execute(
         """
@@ -19,8 +30,10 @@ def get(latitude, longitude, bearing):
             ST_Intersects(ST_Transform(
             ST_Segmentize(
                 ST_MakeLine(
-                ST_Transform(ST_SetSRID(ST_MakePoint({longitude}, {latitude}), 4326), 953027),
-                ST_Transform(ST_SetSRID(ST_AsText(ST_Project(ST_MakePoint(0.1278, 51.5074), 10000000, radians({bearing}))), 4326), 953027)
+                -- ST_Transform(ST_SetSRID(ST_MakePoint({longitude}, {latitude}), 4326), 953027),
+                -- ST_Transform(ST_SetSRID(ST_AsText(ST_Project(ST_MakePoint(0.1278, 51.5074), 10000000, radians({bearing}))), 4326), 953027)
+                ST_SetSRID(ST_MakePoint({longitude}, {latitude}), 4326),
+                ST_SetSRID(ST_AsText(ST_Project(ST_MakePoint(0.1278, 51.5074), 10000000, radians({bearing}))), 4326)
                 ), 
             1000000), 
             4326), area)
