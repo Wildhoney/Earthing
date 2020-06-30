54b57e9b7ab3c3898b325079e17d4ea3bc6a7749
Wildhoney <adam.timberlake@gmail.com>
2020-06-30 23:12:48 +0100

    Removed unused component and renamed another to TS

--- src/components/Loading/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Text } from 'react-native';
-import style from './styles';
-
-export default function Loading() {
-    return <Text style={style.container}>Loading&hellip;</Text>;
-}
