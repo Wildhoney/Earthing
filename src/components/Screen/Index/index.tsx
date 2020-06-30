a5b23831c97c2599807c2e932055349938a72c5a
Wildhoney <adam.timberlake@gmail.com>
2020-06-30 15:39:17 +0100

    Added initial Magnetometer implementation

--- src/components/Screen/Index/index.tsx
+++ src/components/Screen/Index/index.tsx
@@ -1,25 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useAsync from 'react-use/lib/useAsync';
 import { camelizeKeys } from 'humps';
 import { SafeAreaView, Text, ScrollView } from 'react-native';
 import { API_URL } from 'dotenv';
 import url from 'url-join';
+import { Magnetometer } from 'expo-sensors';
 import Place from '../../Place';
 import { Model as PlaceModel } from '../../Place/types';
 import Loading from '../../Loading';
 import Error from '../../Error';
 import * as t from './types';
 import getStyles from './styles';
+import * as utils from './utils';
+
+Magnetometer.setUpdateInterval(1000);
 
 export default function App() {
-    const heading = 90;
+    const [compassHeading, setCompassHeading] = useState(90);
+
+    useEffect(() => {
+        const subscription = Magnetometer.addListener((result) => setCompassHeading(result.x));
+        return () => subscription.remove();
+    }, []);
 
     const places: t.Places = useAsync(async () => {
-        const response = await fetch(url(API_URL, `51.5074/0.1278/${heading}`)).then((response) =>
-            response.json()
-        );
+        const response = await fetch(
+            url(API_URL, `51.5074/0.1278/${compassHeading}`)
+        ).then((response) => response.json());
         return camelizeKeys(response);
-    });
+    }, [utils.toNearestTen(compassHeading)]);
 
     const style = getStyles({ isLoading: places.loading });
 
@@ -34,8 +43,8 @@ export default function App() {
                     ) : (
                         <>
                             <Text style={style.description}>
-                                Walking {heading}˚ of your current location in a straight line would
-                                take you through the following countries:
+                                Walking {Math.round(compassHeading)}˚ of your current location in a
+                                straight line would take you through the following countries:
                             </Text>
 
                             <ScrollView style={style.scroll}>
